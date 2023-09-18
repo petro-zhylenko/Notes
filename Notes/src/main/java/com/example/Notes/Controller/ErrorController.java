0a12d562a09ea20ed559ee01d56469ea448b5293
AlexandrMateush <128997943+AlexandrMateush@users.noreply.github.com>
2023-09-18 20:06:05 +0300

    Update ErrorController.java

--- Notes/src/main/java/com/example/Notes/Controller/ErrorController.java
+++ Notes/src/main/java/com/example/Notes/Controller/ErrorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
     
+    
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
